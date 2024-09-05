7c61e60edcdf788d4b3f0ac268f20cbf69d417c0
priyanshuahir000 <priyanshuahir01@gmail.com>
2024-09-05 11:16:23 +0530

    add otp verification function

--- server/routes/user.routes.ts
+++ server/routes/user.routes.ts
@@ -1,7 +1,9 @@
 import express from "express";
-import { registrationUser } from "../controllers/user.controller";
+import { registrationUser, activateUser } from "../controllers/user.controller";
 const userRouter = express.Router();
 
 userRouter.post("/register", registrationUser);
+
+userRouter.post("/activateUser", activateUser);
 
 export default userRouter;
